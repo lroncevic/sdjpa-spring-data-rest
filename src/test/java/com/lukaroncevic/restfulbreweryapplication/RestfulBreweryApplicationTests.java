22ba32853956223253212ca0d7d4c2669e5aa4e9
lroncevic <roncevicluka@outlook.com>
2023-04-01 23:11:11 +0200

    Initial project add

---
+++ src/test/java/com/lukaroncevic/restfulbreweryapplication/RestfulBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukaroncevic.restfulbreweryapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
